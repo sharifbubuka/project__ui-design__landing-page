81896d0ed173017cef34222070e60a5c1df919f1
Bubuka Sharif <sharifbubuka256@gmail.com>
2022-01-29 00:57:33 +0300

    added header and banner-section

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,15 +1,38 @@
 /** @jsx jsx */
-import { jsx, Container, Flex, Button } from 'theme-ui';
-import { keyframes } from '@emotion/core';
-import { Link } from 'react-scroll';
-import Logo from 'components/logo';
-import LogoDark from 'assets/logo.svg';
-import MobileDrawer from './mobile-drawer';
-import menuItems from './header.data';
+import { jsx, Container, Flex, Button } from "theme-ui";
+import { keyframes } from "@emotion/core";
+import { Link } from "react-scroll";
+import Logo from "components/logo";
+import LogoDark from "assets/logo.svg";
+import MobileDrawer from "./mobile-drawer";
+import menuItems from "./header.data";
 
 export default function Header({ className }) {
   return (
-      <h1>Header</h1>
+    <header sx={styles.header} className={className}>
+      <Container sx={styles.container}>
+        <Logo src={LogoDark} />
+        <Flex as="nav" sx={styles.nav}>
+          {menuItems.map((item, index) => (
+            <Link
+              activeClass="active"
+              to={item.path}
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              key={index}
+            >
+              {item.label}
+            </Link>
+          ))}
+        </Flex>
+        <Button className="donate__btn" variant="secondary" aria-label="Get Started">
+          Get Started
+        </Button>
+        <MobileDrawer />
+      </Container>
+    </header>
   );
 }
 
@@ -28,55 +51,55 @@ const positionAnim = keyframes`
 
 const styles = {
   header: {
-    color: 'text',
-    fontWeight: 'body',
+    color: "text",
+    fontWeight: "body",
     py: 4,
-    width: '100%',
-    position: 'absolute',
+    width: "100%",
+    position: "absolute",
     top: 0,
     left: 0,
-    backgroundColor: 'transparent',
-    transition: 'all 0.4s ease',
+    backgroundColor: "transparent",
+    transition: "all 0.4s ease",
     animation: `${positionAnim} 0.4s ease`,
-    '.donate__btn': {
+    ".donate__btn": {
       flexShrink: 0,
       mr: [15, 20, null, null, 0],
-      ml: ['auto', null, null, null, 0],
+      ml: ["auto", null, null, null, 0],
     },
-    '&.sticky': {
-      position: 'fixed',
-      backgroundColor: 'background',
-      color: '#000000',
-      boxShadow: '0 1px 2px rgba(0, 0, 0, 0.06)',
+    "&.sticky": {
+      position: "fixed",
+      backgroundColor: "background",
+      color: "#000000",
+      boxShadow: "0 1px 2px rgba(0, 0, 0, 0.06)",
       py: 3,
-      'nev > a': {
-        color: 'text',
+      "nev > a": {
+        color: "text",
       },
     },
   },
   container: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   nav: {
-    mx: 'auto',
-    display: 'none',
-    '@media screen and (min-width: 1024px)': {
-      display: 'block',
+    mx: "auto",
+    display: "none",
+    "@media screen and (min-width: 1024px)": {
+      display: "block",
     },
     a: {
       fontSize: 2,
-      fontWeight: 'body',
+      fontWeight: "body",
       px: 5,
-      cursor: 'pointer',
-      lineHeight: '1.2',
-      transition: 'all 0.15s',
-      '&:hover': {
-        color: 'primary',
+      cursor: "pointer",
+      lineHeight: "1.2",
+      transition: "all 0.15s",
+      "&:hover": {
+        color: "primary",
       },
-      '&.active': {
-        color: 'primary',
+      "&.active": {
+        color: "primary",
       },
     },
   },

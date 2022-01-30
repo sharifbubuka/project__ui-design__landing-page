0331ab86a0d898246ed614bef52e2c75a493242f
Abdirahim Ahmed <abdirahimiahmed@gmail.com>
2022-01-30 07:09:35 +0300

    [FEAT] added iframe to display youtube videos (#1)

--- src/sections/service-section.js
+++ src/sections/service-section.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 /** @jsx jsx */
 import {
   jsx,
@@ -9,51 +9,48 @@ import {
   Heading,
   Button,
   Image,
-} from "theme-ui";
-import { keyframes } from "@emotion/core";
-import TextFeature from "components/text-feature";
-import ModalVideo from "react-modal-video";
-import { IoIosPlay } from "react-icons/io";
+} from 'theme-ui';
+import { keyframes } from '@emotion/core';
+import TextFeature from 'components/text-feature';
+import { IoIosPlay } from 'react-icons/io';
 
-import ServiceThumb from "assets/service-thumb.png";
-import shapePattern from "assets/shape-pattern1.png";
+import ServiceThumb from 'assets/service-thumb.png';
+import shapePattern from 'assets/shape-pattern1.png';
 
-import Smart from "assets/services/smart.svg";
-import Secure from "assets/services/secure.svg";
+import Smart from 'assets/services/smart.svg';
+import Secure from 'assets/services/secure.svg';
 
 const data = {
-  subTitle: "our services",
-  title: "Business Goals Achieved with Design",
+  subTitle: 'our services',
+  title: 'Business Goals Achieved with Design',
   features: [
     {
       id: 1,
       imgSrc: Smart,
-      altText: "Smart Features",
-      title: "Smart Features",
-      text:
-        "Get your blood tests delivered at let home collect sample from the victory of the managements. your blood tests.",
+      altText: 'Smart Features',
+      title: 'Smart Features',
+      text: 'Get your blood tests delivered at let home collect sample from the victory of the managements. your blood tests.',
     },
     {
       id: 2,
       imgSrc: Secure,
-      altText: "Secure Contents",
-      title: "Secure Contents",
-      text:
-        "Get your blood tests delivered at let home collect sample from the victory of the managements. your blood tests.",
+      altText: 'Secure Contents',
+      title: 'Secure Contents',
+      text: 'Get your blood tests delivered at let home collect sample from the victory of the managements. your blood tests.',
     },
   ],
 };
 
 export default function ServiceSection() {
-  const [videoOpen, setVideoOpen] = useState(false)
+  const [videoOpen, setVideoOpen] = useState(false);
 
-  const handleClick = (e) => {
+  const handleClick = e => {
     e.preventDefault();
     setVideoOpen(true);
   };
 
   return (
-    <section sx={{ variant: "section.services" }}>
+    <section sx={{ variant: 'section.services' }}>
       <Container sx={styles.containerBox}>
         <Box sx={styles.thumbnail}>
           <Image src={ServiceThumb} alt="Thumbnail" />
@@ -75,7 +72,11 @@ export default function ServiceSection() {
           <Grid sx={styles.grid}>
             {data.features.map(feature => (
               <Box sx={styles.card} key={feature.id}>
-                <Image src={feature.imgSrc} alt={feature.altText} sx={styles.icon} />
+                <Image
+                  src={feature.imgSrc}
+                  alt={feature.altText}
+                  sx={styles.icon}
+                />
                 <Box sx={styles.wrapper}>
                   <Heading sx={styles.wrapper.title}>{feature.title}</Heading>
                   <Text sx={styles.wrapper.subTitle}>{feature.text}</Text>
@@ -85,12 +86,22 @@ export default function ServiceSection() {
           </Grid>
         </Box>
       </Container>
-      {/* <ModalVideo
-        channel="youtube"
-        isOpen={videoOpen}
-        videId="9Gxs6ZQcCpc"
-        onClose={() => setVideoOpen(false)} 
-      /> */}
+      {videoOpen ? (
+        <div
+          className="video-display-wrapper"
+          sx={styles.video_display_wrapper}
+        >
+          <div
+            className="video_display"
+            sx={styles.video_display_wrapper.video_display}
+          >
+            <iframe src="https://www.youtube.com/embed/ahy5o5nT4oI"></iframe>
+            <span onClick={() => setVideoOpen(false)}>X</span>
+          </div>
+        </div>
+      ) : (
+        ''
+      )}
     </section>
   );
 }
@@ -110,70 +121,70 @@ const playPluse = keyframes`
 const styles = {
   coreFeature: {
     py: [0, null, null, 2, null, 7],
-    position: "relative",
+    position: 'relative',
   },
   containerBox: {
-    display: "flex",
-    alignItems: ["flex-start", null, null, "center"],
-    justifyContent: ["flex-start", null, null, "space-between"],
-    flexDirection: ["column", null, null, "row"],
+    display: 'flex',
+    alignItems: ['flex-start', null, null, 'center'],
+    justifyContent: ['flex-start', null, null, 'space-between'],
+    flexDirection: ['column', null, null, 'row'],
     pb: [0, null, null, null, null, 7],
   },
   thumbnail: {
-    mr: ["auto", null, null, 6, 60, 85],
+    mr: ['auto', null, null, 6, 60, 85],
     order: [2, null, null, 0],
-    ml: ["auto", null, null, 0],
-    display: "inline-flex",
-    position: "relative",
-    "> img": {
-      position: "relative",
+    ml: ['auto', null, null, 0],
+    display: 'inline-flex',
+    position: 'relative',
+    '> img': {
+      position: 'relative',
       zIndex: 1,
-      height: [310, "auto"],
+      height: [310, 'auto'],
     },
   },
   shapeBox: {
-    position: "absolute",
+    position: 'absolute',
     bottom: -68,
     left: -160,
     zIndex: -1,
-    display: ["none", null, null, null, null, "inline-block"],
+    display: ['none', null, null, null, null, 'inline-block'],
   },
   videoBtn: {
     zIndex: 2,
-    position: "absolute",
-    left: "50%",
-    top: "50%",
-    transform: "translate(-50%, -50%)",
-    width: ["60px", null, "80px", null, "100px"],
-    height: ["60px", null, "80px", null, "100px"],
-    p: "0px !important",
-    backgroundColor: "transparent",
-    "&::before": {
-      position: "absolute",
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: ['60px', null, '80px', null, '100px'],
+    height: ['60px', null, '80px', null, '100px'],
+    p: '0px !important',
+    backgroundColor: 'transparent',
+    '&::before': {
+      position: 'absolute',
       content: '""',
-      left: "50%",
-      top: "50%",
-      transform: "translateX(-50%) translateY(-50%)",
-      display: "block",
-      width: ["60px", null, "80px", null, "100px"],
-      height: ["60px", null, "80px", null, "100px"],
-      backgroundColor: "primary",
-      borderRadius: "50%",
+      left: '50%',
+      top: '50%',
+      transform: 'translateX(-50%) translateY(-50%)',
+      display: 'block',
+      width: ['60px', null, '80px', null, '100px'],
+      height: ['60px', null, '80px', null, '100px'],
+      backgroundColor: 'primary',
+      borderRadius: '50%',
       animation: `${playPluse} 1.5s ease-out infinite`,
       opacity: 0.5,
     },
-    "> span": {
-      backgroundColor: "rgba(255,255,255,0.5)",
-      width: "inherit",
-      height: "inherit",
-      textAlign: "center",
-      borderRadius: "50%",
-      cursor: "pointer",
-      transition: "all 0.5s",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      position: "relative",
+    '> span': {
+      backgroundColor: 'rgba(255,255,255,0.5)',
+      width: 'inherit',
+      height: 'inherit',
+      textAlign: 'center',
+      borderRadius: '50%',
+      cursor: 'pointer',
+      transition: 'all 0.5s',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      position: 'relative',
       zIndex: 2,
     },
     svg: {
@@ -181,67 +192,82 @@ const styles = {
     },
   },
   contentBox: {
-    width: ["100%", null, null, 315, 390, 450, null, 500],
+    width: ['100%', null, null, 315, 390, 450, null, 500],
     flexShrink: 0,
     mb: [7, null, 60, 0],
-    textAlign: ["center", null, null, "left"],
+    textAlign: ['center', null, null, 'left'],
   },
   grid: {
-    pr: [2, 0, null, null, 6, "70px"],
+    pr: [2, 0, null, null, 6, '70px'],
     pl: [2, 0],
     pt: [2, null, null, null, 3],
-    mx: "auto",
-    width: ["100%", 370, 420, "100%"],
-    gridGap: ["35px 0", null, null, null, "50px 0"],
-    gridTemplateColumns: ["repeat(1,1fr)"],
+    mx: 'auto',
+    width: ['100%', 370, 420, '100%'],
+    gridGap: ['35px 0', null, null, null, '50px 0'],
+    gridTemplateColumns: ['repeat(1,1fr)'],
   },
   card: {
-    display: "flex",
-    alignItems: "flex-start",
-    transition: "all 0.3s",
+    display: 'flex',
+    alignItems: 'flex-start',
+    transition: 'all 0.3s',
   },
 
   icon: {
-    width: ["45px", null, null, null, "55px"],
-    height: "auto",
+    width: ['45px', null, null, null, '55px'],
+    height: 'auto',
     flexShrink: 0,
     mr: [3, null, null, null, 4],
   },
   wrapper: {
-    width: "100%",
-    display: "flex",
-    flexDirection: "column",
-    textAlign: "left",
-    mt: "-5px",
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    textAlign: 'left',
+    mt: '-5px',
     title: {
       fontSize: 3,
-      color: "heading_secondary",
+      color: 'heading_secondary',
       lineHeight: 1.4,
       fontWeight: 700,
       mb: [2, null, 3, 2, 3],
     },
 
     subTitle: {
-      fontSize: [1, null, null, "14px", 1],
+      fontSize: [1, null, null, '14px', 1],
       fontWeight: 400,
       lineHeight: 1.9,
     },
   },
-  videoWrapper: {
-    maxWidth: "100%",
-    position: "relative",
-    width: "900px",
-    "&:before": {
-      content: '""',
-      display: "block",
-      paddingTop: "56.25%",
-    },
-    iframe: {
-      width: "100%",
-      height: "100%",
-      position: "absolute",
-      top: 0,
-      left: 0,
+  video_display_wrapper: {
+    backgroundColor: 'rgba(0,0,0,0.1)',
+    position: 'absolute',
+    width: '100%',
+    height: '88%',
+    transform: 'translateY(-650px)',
+    zIndex: '3',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backdropFilter: 'blur(2px)',
+    video_display: {
+      position: 'relative',
+      width: '40rem',
+      height: '30rem',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      iframe: {
+        width: '90%',
+        height: '90%',
+      },
+      span: {
+        position: 'absolute',
+        top: '0',
+        right: '0',
+        fontWeight: 'bold',
+        fontSize: '1.4rem',
+        cursor: 'pointer',
+      },
     },
   },
 };
